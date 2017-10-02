3305597239e7e102a9607bd3ac998a44319fafef
Jack Wood <gritteh@gmail.com>
2017-10-02 13:57:29 +0100

    Navbar stuff

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,18 @@
 $(document).ready(() => {
 
+    let workBlock = $(".top__right");
+    let aboutBlock = $(".bottom__left");
+    let contactBlock = $(".bottom__right");
 
+    workBlock.on("click", () => {
+        window.location = "http://jacks.work/work.html";
+    });
+    aboutBlock.on("click", () => {
+        window.location = "http://jacks.work/about.html";
+    });
+    contactBlock.on("click", () => {
+        window.location = "http://jacks.work/contact.html";
+    });
 
 
     
