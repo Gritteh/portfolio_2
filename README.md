9db6d43ee5becb89e53dbb9f3aa96e1c5fc4c01f
Jack Wood <gritteh@gmail.com>
2017-10-02 13:26:05 +0100

    links on contact page

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # portfolio_2
+
+--- 
+
+Work in progress 
+
+---
---
+++ js/contact.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    let linkedInBlock = $(".linkedin__section");
+    let twitterBlock = $(".twitter__section");
+    let githubBlock = $(".github__section");
+
+    linkedInBlock.on("click", () => {
+        window.open("http://linkedin.com/in/jackjs", "_blank");
+    });
+    twitterBlock.on("click", () => {
+        window.open("http://twitter.com/jackstewartwood", "_blank");
+    });
+    githubBlock.on("click", () => {
+        window.open("http://github.com/gritteh", "_blank");
+    });
+
+});
