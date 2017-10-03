c4fc295be2f795726afd40b38514b7ce8f56bb05
Jack Wood <gritteh@gmail.com>
2017-10-03 06:48:51 +0100

    js

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,14 +3,57 @@ $(document).ready(() => {
     let topLeft = $(".top__left");
     let facePic = $(".face__pic");
     let wind0w = $(window);
-    let screenWidth = wind0w.width();
+    let screenWidth = $(window).width();
 
     let faceWidth = facePic.width();
     let contWidth = topLeft.width();
 
     facePic.css({
         marginLeft: contWidth/2 - faceWidth/2 + "px"
-    })
+    });
+
+    
+    let fork = $(".right__fork");
+    let coffee = $(".left__coffee");
+    let mail = $(".right__mail");
+
+    let forkCont = $(".top__right");
+    let forkContWidth = forkCont.css("width");
+    let forkWidth = fork.css("width");
+    fork.css({
+        marginLeft: +forkContWidth/2 - +forkWidth/2 + "px"
+    });
+
+    // let fork
+
+
+
+    let iconArray = [fork, coffee, mail];
+
+
+    const centerIcons = (array, messedUp) => {
+       
+        if (messedUp) {
+            array.map((val, i) => {
+                let iconWidth = val.width();
+                let parentWidth = val.parent().width();
+                val.css({
+                    marginLeft: parentWidth/2 - iconWidth/2 + 10 + "px"
+                });
+            });   
+        } else {
+            array.map((val, i) => {
+                let iconWidth = val.width();
+                let parentWidth = val.parent().width();
+                val.css({
+                    marginLeft: parentWidth/2 - iconWidth/2 - 10 + "px"
+                });
+            });
+        }   
+    };
+
+    centerIcons(iconArray, true);
+    centerIcons([coffee, mail], false);
 
     wind0w.on("resize", () => {
         screenWidth = wind0w.width();
@@ -19,24 +62,11 @@ $(document).ready(() => {
         facePic.css({
             marginLeft: contWidth/2 - faceWidth/2 + "px"
         });
-        centerIcons(iconArray);
+        centerIcons(iconArray, false);
     
     });
 
-    let iconArray = [$(".right__fork"), $(".left__coffee"), $(".right__mail")];
+   
 
 
-    const centerIcons = array => {
-        array.map((val, i) => {
-            let iconWidth = val.width();
-            let parentWidth = val.parent().width();
-            val.css({
-                marginLeft: parentWidth/2 - iconWidth/2 + "px"
-            });
-        });
-    };
-
-    centerIcons(iconArray);
-
-    
 });
