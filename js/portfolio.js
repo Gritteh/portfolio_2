5fb4bfb516d959ce38de2ef5ff8f3b5b2354b2ac
Jack Wood <gritteh@gmail.com>
2017-10-03 01:32:52 +0100

    lots of stuff

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,18 +1,26 @@
 $(document).ready(() => {
 
-    // let workBlock = $(".top__right");
-    // let aboutBlock = $(".bottom__left");
-    // let contactBlock = $(".bottom__right");
+    let topLeft = $(".top__left");
+    let facePic = $(".face__pic");
+    let wind0w = $(window);
+    let screenWidth = wind0w.width();
 
-    // workBlock.on("click", () => {
-    //     window.location = "http://jacks.work/work.html";
-    // });
-    // aboutBlock.on("click", () => {
-    //     window.location = "http://jacks.work/about.html";
-    // });
-    // contactBlock.on("click", () => {
-    //     window.location = "http://jacks.work/contact.html";
-    // });
+    let faceWidth = facePic.width();
+    let contWidth = topLeft.width();
+
+    facePic.css({
+        marginLeft: contWidth/2 - faceWidth/2 + "px"
+    })
+
+    wind0w.on("resize", () => {
+        screenWidth = wind0w.width();
+        contWidth = topLeft.width();
+        
+        facePic.css({
+            marginLeft: contWidth/2 - faceWidth/2 + "px"
+        });
+
+    });
 
 
     
