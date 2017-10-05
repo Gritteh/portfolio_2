c0696061886acbb992c9ee04641d83a17d27b0d0
Jack Wood <gritteh@gmail.com>
2017-10-05 12:51:30 +0100

    about page more responsive

--- js/about.js
+++ js/about.js
@@ -9,15 +9,31 @@ $(document).ready(function() {
     wind0w.on("resize", () => {
         screenWidth = wind0w.width();
 
-        if (screenWidth < 540) {
+        blockFullWidth();
+        
+    });
+//                 padding: 4vh 3vw;
+
+    const blockFullWidth = () => {
+        if (screenWidth <= 580) {
             imageContainer.css("display", "none");
             topTitle.addClass("col-12").removeClass("col-8");
+            topTitle.css({
+                fontSize: "2.5em",
+                padding: "8vh 3vw"
+            });
         } else {
             imageContainer.css("display", "");
             topTitle.addClass("col-8").removeClass("col-12");
-            
+            topTitle.css({
+                fontSize: "3em",
+                padding: "4vh 3vw"
+            });
         }
-    });
+    }
+
+    blockFullWidth();
+    
 
     
 
